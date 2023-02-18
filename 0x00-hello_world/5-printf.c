da90682500244b0a7508072dc5116b7783354fa8
Ruvimbo Kaduku <ruvimbokaduku@gmail.com>
2023-02-18 15:45:03 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main -A program that prints a line usin the printf f
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+			return (0);
+			}
