9bd78e75a90d82b55634e8b6def635994ce2bf48
Ruvimbo Kaduku <ruvimbokaduku@gmail.com>
2023-02-18 15:30:55 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (Suceess);
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
+
+
