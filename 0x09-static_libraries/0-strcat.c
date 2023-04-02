95d3c933ee44b549d36144dde032e73c869c2dda
Ruvimbo Kaduku <ruvimbokaduku@gmail.com>
2023-04-02 04:31:15 -0700

    0-strcat

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -4,13 +4,12 @@
  * @dest: input value
  * @src: input value
  *
- * Return: void	
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
-
 
 	i = 0;
 	while (dest[i] != '\0')
@@ -19,12 +18,11 @@ char *_strcat(char *dest, char *src)
 	}
 	j = 0;
 	while (src[j] != '\0')
-{
+	{
 		dest[i] = src[j];
 		i++;
 		j++;
-	}		
-
+	}
 
 	dest[i] = '\0';
 	return (dest);
