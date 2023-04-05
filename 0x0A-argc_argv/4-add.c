dd6fe18b6d0296b10307370cdafd5903364cbf1e
Ruvimbo Kaduku <ruvimbokaduku@gmail.com>
2023-04-05 15:18:08 -0700

    4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,23 +13,18 @@
 		/*Declaring variables*/
 		unsigned int count;
 
-
 		count = 0;
 		while (count < strlen(str)) /*count string*/
-
-
 		{
 			if (!isdigit(str[count])) /*check if str there are digit*/
 			{
 				return (0);
 			}
 
-
 			count++;
 		}
 		return (1);
 	}
-
 
 	/**
 	 * main - Print the name of the program
@@ -39,30 +34,22 @@
 	 * Return: Always 0 (Success)
 	 */
 
-
 	int main(int argc, char *argv[])
 
-
 	{
-
-
 		/*Declaring variables*/
 		int count;
 		int str_to_int;
-int sum = 0;
-
+		int sum = 0;
 
 		count = 1;
 		while (count < argc) /*Goes through the whole array*/
 		{
 			if (check_num(argv[count]))
-
-
 			{
 				str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 				sum += str_to_int;
 			}
-
 
 			/*Condition if one of the number contains symbols that are not digits*/
 			else
@@ -71,13 +58,10 @@ int sum = 0;
 				return (1);
 			}
 
-
 			count++;
 		}
 
-
 		printf("%d\n", sum); /*print sum*/
-
 
 		return (0);
 	}
