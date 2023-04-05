64e51c2574cc6651d5ae7db7725f03b6b5af9010
Ruvimbo Kaduku <ruvimbokaduku@gmail.com>
2023-04-05 04:24:39 -0700

    5-strstr

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -12,19 +12,14 @@ char *_strstr(char *haystack, char *needle)
 		char *l = haystack;
 		char *p = needle;
 
-
 		while (*l == *p && *p != '\0')
 		{
 			l++;
 			p++;
 		}
 
-
 		if (*p == '\0')
 			return (haystack);
 	}
-
-
 	return (0);
 }
-
